2023026dd373e997c7cfbcc2a5e5558684eb0244
Sepect <39028541+Sepect@users.noreply.github.com>
2024-11-02 03:57:13 +0800

    Update TunisCss.js

--- layouts/TunisCss.js
+++ layouts/TunisCss.js
@@ -1,12 +1,10 @@
 "use client";
+import { useContext } from "react";
 import { TunisContext } from "@/context/context";
-import { Fragment, useContext } from "react";
+
 const TunisCss = () => {
   const { color } = useContext(TunisContext);
-  return (
-    <Fragment>
-      <link rel="stylesheet" href={`/assets/css/skins/${color}.css`} />
-    </Fragment>
-  );
+  return <link rel="stylesheet" href={`/assets/css/skins/${color}.css`} />;
 };
+
 export default TunisCss;
